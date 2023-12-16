72ec5c549e87f8804b54e5f196e6b387c35a67b9
vineethsankre <lsvineethsankre@gmail.com>
2023-12-16 07:18:32 +0000

    Attrites and methods declared

---
+++ src/main/java/com/example/todo/TodoController.java
@@ -0,0 +1,9 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ * 
+ */
+
+// Write your code here
---
+++ src/main/java/com/example/todo/TodoService.java
@@ -0,0 +1,35 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.web.server.ResponseStatusException;
+ * import org.springframework.http.HttpStatus;
+ * 
+ */
+
+package com.example.todo;
+
+import com.example.todo.Todo;
+import com.example.todo.TodoRepository;
+
+import java.util.*;
+
+// Do not modify the below code
+
+public class TodoService implements TodoRepository {
+
+    private static HashMap<Integer, Todo> todoList = new HashMap<>();
+
+    public TodoService() {
+        todoList.put(1, new Todo(1, "Watch Movie", "LOW", "TO DO"));
+        todoList.put(2, new Todo(2, "Finish Project", "HIGH", "IN PROGRESS"));
+        todoList.put(3, new Todo(3, "Buy Groceries", "MEDIUM", "TO DO"));
+        todoList.put(4, new Todo(4, "Learning from NxtWave", "HIGH", "IN PROGRESS"));
+        todoList.put(5, new Todo(5, "Go for a Run", "MEDIUM", "DONE"));
+
+    }
+
+    // Do not modify the above code
+
+    // Write your code here
+
+}
